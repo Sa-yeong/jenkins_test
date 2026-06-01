cb31445961b93070b92b11eb9788492c74ff5773
C235044 <imsiho@naver.com>
2026-06-01 16:18:03 +0900

    나현 메일전송확인테스트

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("add Something");
+        System.out.println("add Something2");
     }
 
 }git pull origin maingit pull origin main
